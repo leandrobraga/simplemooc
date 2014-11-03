a19367dd4ece914b24f931554aebf7ddb6789f40
leandro <leandro.quadros@gmail.com>
2014-11-03 20:53:22 -0200

    Add app courses

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'simplemooc.core',
+    'simplemooc.courses',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
