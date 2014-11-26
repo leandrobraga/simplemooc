1cc6888ce622270def200fb92ee86a344381cc8e
Leandro Braga <leandro.quadros@gmail.com>
2014-11-26 13:57:01 -0200

    Add form contact

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from simplemooc.courses.models import Course
+from .forms import ContactCourse
 
 
 def index(request):
@@ -25,8 +26,15 @@ def detail(request, slug):
 
     course = get_object_or_404(Course, slug=slug)
 
+    if request.method == "POST":
+
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
+
     context = {
-        'course': course
+        'course': course,
+        'form': form
     }
 
     return render(request, 'courses/detail.html', context)
