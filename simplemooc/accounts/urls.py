22a416e43f18f26fdea241c9b00898c4d7587611
Leandro Braga <leandro.quadros@gmail.com>
2014-12-18 23:17:11 -0200

    Form User

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^entrar/', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'},name='index'),
+    url(r'^entrar/', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='index'),
+    url(r'^registre-se/', 'simplemooc.accounts.views.register', name='register'),
 )
