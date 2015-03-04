75c9c0b1afd3037807173c6c35588900f92b22a5
Leandro <leandro.quadros@gmail.com>
2015-03-04 14:57:51 -0300

    reset senhaform

--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from .forms import EditAccountForm
+from .forms import PasswordResetForm
+from .models import PasswordReset
+from simplemooc.core.utils import generate_hash_key
 from django.conf import settings
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+
+
+User = get_user_model()
 
 
 @login_required
@@ -30,6 +38,21 @@ def register(request):
         form = RegisterForm()
 
     context = {'form': form}
+
+    return render(request, template_name, context)
+
+
+def password_reset(request):
+    template_name = "accounts/reset_password.html"
+    context = {}
+    form = PasswordResetForm(request.POST or None)
+    if form.is_valid():
+        user = User.objects.get(email=form.cleaned_data['email'])
+        key = generate_hash_key(user.username)
+        reset = PasswordReset(key=key, user=user)
+        reset.save()
+        context['success'] = True
+    context['form'] = form
 
     return render(request, template_name, context)
 
